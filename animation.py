6dbe4c28edede79d2fdc080fec09e20dc429a7ac
sossgrammer <142520725+sossgrammer@users.noreply.github.com>
2023-09-26 11:18:34 -0400

    animation.py

---
+++ animation.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+This example uses AnimationsSequence to display multiple animations in sequence, at a five second
+interval.
+
+For NeoPixel FeatherWing. Update pixel_pin and pixel_num to match your wiring if using
+a different form of NeoPixels.
+"""
+import board
+import neopixel
+
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+from adafruit_led_animation.animation.solid import Solid
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.color import (
+RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE)
+
+# Update to match the pin connected to your NeoPixels
+pixel_pin = board.D2
+# Update to match the number of NeoPixels you have connected
+pixel_num = 30
+
+pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.5, auto_write=False)
+
+rainbow = Rainbow(pixels, speed=0.01, period=2)
+rainbow_chase = RainbowChase(pixels, speed=0.05, size=5, spacing=3)
+rainbow_comet = RainbowComet(pixels, speed=0.025, tail_length=7, bounce=True)
+rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
+
+
+animations = AnimationSequence(
+    rainbow,
+    rainbow_chase,
+    rainbow_comet,
+    rainbow_sparkle,
+    advance_interval=5,
+    auto_clear=True,
+)
+
+while True:
+    animations.animate()
